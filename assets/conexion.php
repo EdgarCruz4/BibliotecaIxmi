bd3e8155bc7631449fb4fe387be741d3b9f71187
Edgar Cruz <edgarcruztrejo0@gmail.com>
2023-03-26 21:35:56 -0600

    conexion

---
+++ assets/conexion.php
@@ -0,0 +1,10 @@
+<?php 
+	date_default_timezone_set('America/Mexico_City');
+	$servidor="";
+
+	$usuario="";
+	$contrasena="";
+	$bd="";
+
+    $conexion= mysqli_connect($servidor,$usuario,$contrasena,$bd);
+ ?>
