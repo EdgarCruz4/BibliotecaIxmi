7dab56dd00706cf6cfc844d49fee092692562351
Edgar Cruz <edgarcruztrejo0@gmail.com>
2023-03-26 21:46:11 -0600

    configuracion de la bd

--- assets/conexion.php
+++ assets/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 	date_default_timezone_set('America/Mexico_City');
-	$servidor="";
+	$servidor="MYSQL5045.site4now.net";
 
-	$usuario="";
-	$contrasena="";
-	$bd="";
+	$usuario="a96468_audit";
+	$contrasena="Aud1t0r1@";
+	$bd="db_a96468_audit";
 
     $conexion= mysqli_connect($servidor,$usuario,$contrasena,$bd);
  ?>
